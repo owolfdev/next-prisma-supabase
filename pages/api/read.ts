6fafcbe44236797261003e40fdcc01d4abd8d2b7
mrordinay <ordinarycodeemail@gmail.com>
2022-09-12 04:42:45 +0700

    update

--- pages/api/read.ts
+++ pages/api/read.ts
@@ -3,8 +3,9 @@ import { getUsers } from "../../helpers/prisma-helper";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   async function main() {
-    const user = await getUsers();
-    res.end(JSON.stringify(user));
+    // const users = await getUsers();
+    const users = [{ id: 1 }, { id: 2 }, { id: 3 }];
+    res.end(JSON.stringify(users));
   }
   main();
   res.status(200);
